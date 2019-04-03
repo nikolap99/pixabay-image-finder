d4b8dacfaddc7a0d084175ea14b1d6912fcba6fa
Nikola <nikolapavlov33@gmail.com>
2019-04-03 04:25:32 +0200

    initial app searching

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import NavBar from "./components/NavBar/NavBar";
+import Search from "./components/Search/Search";
+
 import "./App.css";
 
 class App extends Component {
     render() {
-        return <div className="App">Hello</div>;
+        return (
+            <MuiThemeProvider>
+                <div>
+                    <NavBar />
+                    <Search />
+                </div>
+            </MuiThemeProvider>
+        );
     }
 }
 
